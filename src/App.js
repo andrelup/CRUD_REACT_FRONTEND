cb0c082ce58d60085ca9c60990a33cda7ea2700a
André Pereira <andrelcop@gmail.com>
2024-10-03 18:57:15 +0200

    update the login method to connect to the backend

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   state = {
     showBackdrop: false,
     showMobileNav: false,
-    isAuth: true,
+    isAuth: false,
     token: null,
     userId: null,
     authLoading: false,
@@ -59,8 +59,15 @@ class App extends Component {
   loginHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('URL')
-      .then(res => {
+    fetch('http://localhost:8080/auth/login', {
+      'method': 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+      })
+    }).then(res => {
         if (res.status === 422) {
           throw new Error('Validation failed.');
         }
@@ -103,14 +110,14 @@ class App extends Component {
     fetch('http://localhost:8080/auth/signup', {
       method: 'PUT',
       headers: {
-        'Content-Type': 'aplication/json'
-      }, body: JSON.stringify({
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify({
         email: authData.signupForm.email.value,
         password: authData.signupForm.password.value,
         name: authData.signupForm.name.value
       })
-    })
-      .then(res => {
+    }).then(res => {
         if (res.status === 422) {
           throw new Error(
             "Validation failed. Make sure the email address isn't used yet!"
